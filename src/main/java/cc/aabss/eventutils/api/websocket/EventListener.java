10c52514115d464d1ccafc52a39b05b9bc4d4385
aabssmc <lilmikeyninja203@gmail.com>
2024-06-09 21:10:42 -0700

    remove minimum prize cuz it doesnt even work

--- src/main/java/cc/aabss/eventutils/api/websocket/EventListener.java
+++ src/main/java/cc/aabss/eventutils/api/websocket/EventListener.java
@@ -5,7 +5,6 @@ import cc.aabss.eventutils.api.NotificationToast;
 import cc.aabss.eventutils.config.EventUtil;
 import com.google.gson.JsonObject;
 
-import static cc.aabss.eventutils.EventUtils.MINIMUM_PRIZE;
 import static cc.aabss.eventutils.config.EventUtil.*;
 import static cc.aabss.eventutils.config.EventUtil.prize;
 
@@ -38,11 +37,8 @@ public class EventListener {
 
     public void onMoneyEvent(JsonObject message) {
         if (EventUtils.MONEY_EVENT){
-            int prize = prize(message);
-            if (prize >= MINIMUM_PRIZE) {
-                NotificationToast.addMoneyEvent(prize);
-                LAST_MONEY_IP = getAndConnectIP(message);
-            }
+            NotificationToast.addMoneyEvent(prize(message));
+            LAST_MONEY_IP = getAndConnectIP(message);
         }
     }
 
